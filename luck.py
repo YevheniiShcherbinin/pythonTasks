6a8a5c290f87e9e61bcf17047013e84cb0624ad1
YevheniiShcherbinin <yevhenii.shcherbinin.cr@gmail.com>
2020-09-03 05:10:01 +0300

    Test

--- luck.py
+++ luck.py
@@ -13,7 +13,7 @@ cnt = 0
 for i in range(a, z+1):
     i_str = str(i)
     while len(i_str) < 6:
-        i_str = '0' + i_str
+        i_str = "0" + i_str
     i = list(i_str)
     f = i[0:3]
     s = i[3:]
