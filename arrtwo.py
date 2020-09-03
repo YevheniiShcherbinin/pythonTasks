57c010ed06c3747de9e6428a01fff5b7a55c5c1a
YevheniiShcherbinin <yevhenii.shcherbinin.cr@gmail.com>
2020-09-03 05:10:48 +0300

    Test

--- arrtwo.py
+++ arrtwo.py
@@ -1,11 +1,16 @@
 import random
 
-arr = []
-for i in range(1000):
-    arr.append(random.randint(-1000, 1000))
-    max1 = max(arr)
-    max2 = min(arr)
-for n in arr:
-    if n > max2 and n != max1:
-        max2 = n
-print(max1, max2)
+
+class arrtwo():
+    def __init__(self):
+        pass
+
+    def arr(self, arr, max1, max2):
+        for i in range(1000):
+            arr.append(random.randint(-1000, 1000))
+        max1 = max(arr)
+        max2 = min(arr)
+        for n in arr:
+            if n > max2 and n != max1:
+                max2 = n
+                return print(max1, max2)
