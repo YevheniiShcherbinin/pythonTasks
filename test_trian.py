9fc5a06a1885cdef29024f75eb693fd2bba77f3a
YevheniiShcherbinin <yevhenii.shcherbinin.cr@gmail.com>
2020-09-03 04:14:45 +0300

    Test

--- test_trian.py
+++ test_trian.py
@@ -7,17 +7,39 @@
 
 # def test_kind():
 # assert True
+
 import unittest
-from trian import Triangle
+
+from unittest import TestCase
+import trian
 
 
 class TestTriangle(unittest.TestCase):
 
     def setUp(self):
-        self.Triangle = Triangle()
+        self.Triangle = trian.Triangle()
 
-    def area(self):
+    def test_area(self):
         self.assertEqual(self.Triangle.area(4, 6, 7), 8)
 
-        if __name__ == "__main__":
-            unittest.main()
+    def test_perimeter(self):
+        self.assertEqual(self.Triangle.perimeter(10, 12, 24), 46)
+
+    def test_kind(self):
+        self.assertEqual(self.Triangle.kind(8, 8, 10), "Isosceles")
+
+    def test_exist(self):
+        self.assertEqual(self.Triangle.kind(5, 3, 1), "Triangle not exist")
+
+    def test_Versatile(self):
+        self.assertEqual(self.Triangle.kind(12, 7, 4), "Versatile")
+
+    def test_Rectangular(self):
+
+        self.assertEqual(self.Triangle.kind(3, 4, 5), "Rectangular")
+
+    def test_Equilateral(self):
+        self.assertEqual(self.Triangle.kind(5, 5, 5), "Equilateral")
+
+    if __name__ == "__main__":
+        unittest.main()
